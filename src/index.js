cd13376669adab379ac6efc84570a332bec6e38d
taka2929029 <123882466+taka2929029@users.noreply.github.com>
2023-04-02 13:41:45 +0900

    追加ボタンの追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //divの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  //liタグの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの雇用に各要素を設定
+  div.appendChild(li);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
