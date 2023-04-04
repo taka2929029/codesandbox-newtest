1880e4fb7fd79fec40bbf6834bdbc7efd89645f6
taka2929029 <123882466+taka2929029@users.noreply.github.com>
2023-04-05 00:18:36 +0900

    JS追加

--- src/index.js
+++ src/index.js
@@ -14,8 +14,18 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //button(完了)タグ生成
+  const completebutton = document.createElement("button");
+  completebutton.innerText = "完了";
+
+  //button(削除)タグ生成
+  const deletebutton = document.createElement("button");
+  deletebutton.innerText = "削除";
+
   //divタグの雇用に各要素を設定
   div.appendChild(li);
+  div.appendChild(completebutton);
+  div.appendChild(deletebutton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
